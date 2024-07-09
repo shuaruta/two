76d2b0347473ecfb62b5485a57d1cf91e60139aa
Takuya Nishimoto <nishimotz@gmail.com>
2024-07-09 09:54:07 +0900

    initial

---
+++ Two/ImmersiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ImmersiveView.swift
+//  Two
+//
+//  Created by Takuya Nishimoto on 2024/06/26.
+//
+
+import SwiftUI
+
+struct ImmersiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImmersiveView()
+}
---
+++ Two/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Two
+//
+//  Created by Takuya Nishimoto on 2024/06/28.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
