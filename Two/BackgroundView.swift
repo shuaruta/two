c1892c6af2cde7429e0013b6ae891d8194056709
Takuya Nishimoto <nishimotz@gmail.com>
2024-07-22 12:16:13 +0900

    snapshot

---
+++ Two/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Two
+//
+//  Created by Takuya Nishimoto on 2024/07/21.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
